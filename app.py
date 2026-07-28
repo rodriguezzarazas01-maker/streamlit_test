e1405dd12199468d8498cb5a463ba658f3fbd56e
Santiago Rodríguez Zaraza <rodriguezzarazas01@gmail.com>
2026-07-27 20:37:33 -0500

    Actualizar app inicial de Streamlit

--- app.py
+++ app.py
@@ -1,8 +1,12 @@
 import streamlit as st
 import pandas as pd
 from scipy import stats
-
+import streamlit as st
 st.title("Mi primera aplicación con Streamlit")
 
 st.write("Hola, esta es mi aplicación inicial.")
 
+
+st.header("Lanzar una moneda")
+
+st.write("Esta aplicación aún no es funcional. En construcción.")
